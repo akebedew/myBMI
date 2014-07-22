68d5e20cc424d0aabf398e3a521d3e1ea1afbf67
akebede <adugna.kebede@ncdenr.gov>
2014-07-22 11:17:44 -0400

    MyBMI Version

--- ui.R
+++ ui.R
@@ -10,20 +10,28 @@ fluidPage(
         numericInput('height', 'Height', 70, 5),
         numericInput('age', 'Age', 20, 0),
         numericInput('gender', 'Gender', 0, 0),
-        submitButton('Calc')
+        submitButton('Calc'),
+        br(),
+        br(),
+	  img(src="31164_orig.jpg", height = 920, width = 467),
+        h6('The image below shows weight classification based on BMI for males: '),
+	  img(src="bmi-male.jpg", height = 920, width = 467),
+        h6('The image below shows weight classification based on BMI for females: '),
+	  img(src="bmi-female.jpg", height = 920, width = 467),
+	  h6(em('Image source:  http://www.scientificpsychic.com/fitness/diet.html '))
         ),
     
     mainPanel(
 	  h4('Documentation'),
         p("Monitoring  Body Mass Index (BMI) and percent fat content is important to monitor health status.  
            This application is developed to calculate BMI based on weight and height and peercent  
-           fat content based on BMI, age and gender. The Instruction on how to uss the App. is given below"),
+           fat content based on BMI, age and gender. The images included in the side panel are for illustration only. The Instruction on how to uss the App. is given below"),
         h4('Instructions'),
         p("1. Enter your weight in pounds and height in inches in the form to the 
           left."),
         p("2. Enter your age and gender (for females: gender=0, and for males: gender =1)."),
         p("3. Press the 'Calc' button to see your Body Mass Index (BMI), your % fat content, and your
-          weight status"),
+          weight classification"),
         h4('Your weight in pounds is: '),
         verbatimTextOutput("weight"),
         h4('Your height in inches is: '),
@@ -38,12 +46,7 @@ fluidPage(
         verbatimTextOutput('fatc'),
         h4("Your weight classification is: "),
         verbatimTextOutput('status'),
-	  h6(em('Note: The equations to calculate percent fat content is taken from: http://www.halls.md/bmi/fat.htm')),
-        h6('The image below shows the BMI ranges for males: '),
-	  img(src="bmi-male.jpg", height = 920, width = 467),
-        h6('The image below shows the BMI ranges for females: '),
-	  img(src="bmi-female.jpg", height = 920, width = 467),
-	  h6(em('Image source:  http://www.scientificpsychic.com/fitness/diet.html ')),
+	  h6(em('Note: The equations to calculate percent fat content are taken from: http://www.halls.md/bmi/fat.htm')),
         plotOutput('pie')
         )
     )
